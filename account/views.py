594008ba5323bfc23e36d000252ff05d8ee2312c
Aika1988 <dzhumagulovaaika88@icloud.com>
2023-03-13 13:25:54 +0600

    second

--- account/views.py
+++ account/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from account.serializers import RegisterSerializers, LoginSerializer
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from account.models import *
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializers(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response('Добро пожаловать в Kaynar! Перейдите на почту для подтверждения своего аккаунта.', status=201)
+
+
+class ActivationView(APIView):
+    def get(self, request, activation_code):
+        try:
+            user = User.objects.get(activation_code=activation_code)
+            user.is_active = True
+            user.activation_code = ''
+            user.save()
+            return Response('Успешно', status=200)
+        except User.DoesNotExist:
+            return Response('Срок действия ссылки истек!', status=400)
+
+
+class LoginAPIView(ObtainAuthToken):
+    serializer_class = LoginSerializer
+
+
+class LogoutAPIView(ObtainAuthToken):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        try:
+            user = request.user
+            Token.objects.get(user=user).delete()
+            return Response('Вы успешно разлогинились!', status=200)
+        except:
+            return Response('Ошибка!', status=403)
+
