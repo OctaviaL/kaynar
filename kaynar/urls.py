5338e8265144f6185ac21480bc3a35dc7285d42d
OctaviaL <l.bilmand@gmail.com>
2023-03-14 16:25:39 +0600

    social auth

--- kaynar/urls.py
+++ kaynar/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('api/v1/account/', include('account.urls')),
     path('api/v1/spam/', include('spam.urls')),
     path('api/v1/volunteering/', include('volunteering.urls')),
+    path('social-auth/', include('social_django.urls', namespace="social")),
     # path('api/v1/post/', include('feedback.urls')),
     # path('api/v1/feedback/', include('.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- volunteering/send_email.py
+++ volunteering/send_email.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 
 def send_order_confirmation_code(email, code, name, price):
     
-    full_link = f'Здравствуйте! Подтвердите волонтерство над малышом по имени {name}\n\n http://localhost:8000/api/v1/volunteer/confirm/{code}'
+    full_link = f'Здравствуйте! Подтвердите волонтерство над малышом по имени {name}\n\n http://35.246.210.249/api/v1/volunteer/confirm/{code}'
 
     send_mail(
     'Волонтерство над малышом приюта "Кайнар"',
