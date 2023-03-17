e7a82cf489ef09a9fbc8ea0206e29349f28a301e
eRiKa4530 <omurbekovaerika@gmail.com>
2023-03-17 14:09:53 +0600

    fff

--- post/serializers.py
+++ post/serializers.py
@@ -7,8 +7,6 @@ class PetPostSerializers(serializers.ModelSerializer):
     class Meta:
         model = PetPost
         fields = '__all__'
-   
-   
     def to_representation(self, instance):
             representation = super().to_representation(instance)
             representation['images'] = PetImageSerializers(instance.images.all(), many=True, context=self.context).data
@@ -16,8 +14,6 @@ class PetPostSerializers(serializers.ModelSerializer):
   
 
 class PetImageSerializers(serializers.ModelSerializer):
-
-
     class Meta:
         model = PetImage
         fields = '__all__'
